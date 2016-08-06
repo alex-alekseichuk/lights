09668de0018950ea75448c5a70b33307c668d5c3
Alexander Alexeychuk <kikudjiro@gmail.com>
2016-08-06 11:57:07 +0300

    cells

--- frame_widget.cpp
+++ frame_widget.cpp
@@ -7,18 +7,64 @@ FrameWidget::FrameWidget(QWidget *parent)
 //    setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
 }
 
+void FrameWidget::getCellsByIndex(const int index, QPoint cells[N_PARTS]) {
+    int ix = 1 + (index % SIDE_SIZE);
+    int iy = 1 + (index / SIDE_SIZE);
 
+    cells[0].setX(SIDE_SIZE + ix - 1);
+    cells[0].setY(SIDE_SIZE - iy);
+
+    cells[1].setX(SIDE_SIZE - iy);
+    cells[1].setY(SIDE_SIZE - ix);
+
+    cells[2].setX(SIDE_SIZE - ix);
+    cells[2].setY(SIDE_SIZE + iy - 1);
+
+    cells[3].setX(SIDE_SIZE + iy - 1);
+    cells[3].setY(SIDE_SIZE + ix - 1);
+}
 
 void FrameWidget::paintEvent(QPaintEvent *event)
 {
+
     QPainter painter(this);
     painter.setPen(palette().foreground().color());
     
     int width = size().width();
     int height = size().height();
 
-    painter.drawLine(0, 0, width, height);
-    painter.drawLine(width, 0, 0, height);
+    int len, x0, y0;
+    if (width > height) {
+        len = height;
+        x0 = (width - height) / 2;
+        y0 = 0;
+    } else {
+        len = width;
+        x0 = 0;
+        y0 = (height - width) / 2;
+    }
+    const int MARGIN = 3;
+    int r = (len - MARGIN * (1 + 2 * SIDE_SIZE)) / (2 * SIDE_SIZE);
+
+    int n = SIDE_SIZE * SIDE_SIZE;
+    QPoint cells[N_PARTS];
+    for (int i = 0; i < n; ++i) {
+        getCellsByIndex(i, cells);
+        for (int ic = 0; ic < N_PARTS; ++ic) {
+
+            int x = x0 + (MARGIN + r) * cells[ic].x();
+            int y = y0 + (MARGIN + r) * cells[ic].y();
+
+            painter.setPen(Qt::black);
+            painter.setBrush(Qt::NoBrush);
+
+        //        painter.setPen(Qt::NoPen);
+        //        painter.setBrush(Qt::red);
+
+            painter.drawEllipse(x, y, r, r);
+
+        }
+    }
 
     /*
     if (zoom >= 3) {
--- frame_widget.h
+++ frame_widget.h
@@ -2,18 +2,25 @@
 #define __FRAME_WIDGET_H__
 
 #include <QtGlobal>
+
 #if QT_VERSION >= 0x050000
 #include <QtWidgets>
 #else
 #include <QtGui>
 #endif
 
+#include <QPoint>
+
 class FrameWidget : public QWidget
 {
 public:
     FrameWidget(QWidget *parent = 0);
+    static const int SIDE_SIZE = 4;
+    static const int N_PARTS = 4;
+
 protected:
     void paintEvent(QPaintEvent *event);
+    void getCellsByIndex(const int index, QPoint cells[N_PARTS]);
 };
 
 #endif
